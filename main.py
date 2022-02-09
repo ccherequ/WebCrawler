2bfae151cd430c2ad1d2a8d15e2235600899b5fd
zhihaoc9 <83797177+zhihaoc9@users.noreply.github.com>
2022-02-09 14:19:21 -0800

    Update main.py
    added read_json() function

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import json
 data_struct = {{}}
 from nltk.stem import PorterStemmer
-import json
 
 
 def stem(token):
     stemmer = PorterStemmer()
     return stemmer.stem(token)
 
+
+def read_json():
+    with open("0a0056fb9a53ec6f190aa2b5fb1a97c33cd69726c8841f89d24fa5abd84d276c.json"
+              , "r") as json_file:
+        json_load = json.load(json_file)
+    print(json_load)
