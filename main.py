b845f343fc5935df214197dc5d349dd0e72fa2a4
youngyad <iyadhassan13@gmail.com>
2022-02-09 15:04:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
-#data_struct = {{}}
+data_struct = {{}}
+import nltk
 from nltk.stem import PorterStemmer
-
-
+bp = 'ANALYST/'
+import os
 def stem(token):
     stemmer = PorterStemmer()
     return stemmer.stem(token)
-
 
 def read_json():
     with open("0a0056fb9a53ec6f190aa2b5fb1a97c33cd69726c8841f89d24fa5abd84d276c.json"
@@ -17,3 +17,13 @@ def read_json():
 def make_json_dict(file):
     with open(file,'r') as f:
         return json.load(f)
+
+def read_in(str_path):
+    list_of_files = list()
+    for entry in os.listdir(str_path):
+        x = os.path.join(str_path,entry)
+        for entry2 in os.listdir(x):
+            final = os.path.join(entry2,x)
+            list_of_files.append(final)
+    return list_of_files
+
