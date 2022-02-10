2ebb10d60b7d927c08aef4aacdf0d951d664e1e2
youngyad <iyadhassan13@gmail.com>
2022-02-10 14:54:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,26 +15,22 @@ def stem(token):
     stemmer = PorterStemmer()
     return stemmer.stem(token)
 
+def tokenize(content):
+    Tokenizer = RegexpTokenizer('[a-zA-Z\']{2,}') 
+    tokens = Tokenizer.tokenize(content)
+    return tokens
+
 
 def make_json_dict(file):
     with open(file,'r') as f:
         return json.load(f)
 
 
-def read_in(str_path):
-    list_of_files = list()
-    for entry in os.listdir(str_path):
-        x = os.path.join(str_path,entry)
-        for entry2 in os.listdir(x):
-            final = os.path.join(x,entry2)
-            list_of_files.append(final)
-    return list_of_files
-
-
-def tokenize(content):
-    Tokenizer = RegexpTokenizer('[a-zA-Z\']{2,}') 
-    tokens = Tokenizer.tokenize(content)
-    return tokens
+def writeURLDict():
+    with open('book_keeping.txt', 'a') as file:
+        for key, val in unique_links_dict.items():
+            file.write(key, ' , ', val)
+            file.write("\n")
 
 
 inverted_index = writeBack2File()
@@ -60,8 +56,13 @@ for entry in os.listdir(bp):
         for k,v in tokens_freq.items():
             inverted_index.addUrlToToken(k,counter,v)
 
-print("NUMBER OF INDEXED DOCUMENTS: "+ str(len(unique_links)))
+<<<<<<< HEAD
+print("NUMBER OF INDEXED DOCUMENTS: "+ str(len(unique_links_set)))
 print("NUMBER OF UNIQUE WORDS: "+str(len(inverted_index.data.keys())))
+=======
+print("NUMBER OF INDEXED DOCUMENTS: " + str(len(unique_links_set)))
+print("NUMBER OF UNIQUE WORDS: " + str(len(inverted_index.data.keys())))
+>>>>>>> 8bc644fa2e572095470ad1849c598aaab35a8dd1
 inverted_index.write()
 
 
--- writeBack2File.py
+++ writeBack2File.py
@@ -1,20 +1,31 @@
 import json
-from collections import defaultdict
+from Posting import Posting
 
 class writeBack2File:
     def __init__(self):
-        self.data = defaultdict(list)
-    def addUrlToToken(self, token, url, frequency):
-        # posting = Posting(token, url, frequency)
-        #self.data[token].append(posting)
+        self.data = dict()
+    def addUrlToToken(self, token, docid, fre):
+        P = Posting(docid, fre, 0)
         if token in self.data:
-            self.data[token][url] = frequency
+            self.data[token].append(P)
         else:
-            self.data[token] = dict()
-            self.data[token][url] = frequency
+            self.data[token] = list()
+            self.data[token].append(P)
 
     def write(self):
         with open('report.txt', 'a') as file:
-            file.write(json.dumps(self.data))
-            file.write("\n")
+            for token,lists in tokens_freq.items():
+                file.write(token + "\n")
+                for posting in lists:
+                    file.write(posting.docid + "\n")
+                    file.write(posting.tfidf + "\n")
+                    file.write(posting.file + "\n")
+                file.write("\n")
 
+
+if __name__ == "__main__":
+    w = writeBack2File()
+    w.addUrlToToken("you", "356", "63")
+    w.addUrlToToken("you", "123", "7")
+    w.addUrlToToken("he", "356", "56")
+    w.write()
