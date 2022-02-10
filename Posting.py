7ce08c050c0e6cc932893c390b5efbc64519c4ec
zhihaoc9 <83797177+zhihaoc9@users.noreply.github.com>
2022-02-10 14:39:08 -0800

    Changed wb2f and posting

--- Posting.py
+++ Posting.py
@@ -1,5 +1,5 @@
 class Posting:
-    def __init__(self, docid, tfidf, fileds):
+    def __init__(self, docid, tfidf, fields):
         self.docid = docid
         self.tfidf = tfidf
         self.fields = fields
