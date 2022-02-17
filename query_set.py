b9ee035d8025916d243ee44290c7035037d0ad81
zhihaoc9 <83797177+zhihaoc9@users.noreply.github.com>
2022-02-16 20:46:16 -0800

    Update query_set.py

--- query_set.py
+++ query_set.py
@@ -1,8 +1,31 @@
 
+directory = "indices"
 
 def read_token_index()->dict:
-    file = open("")
+    file = open("token_index", "r")
+    result = dict()
+    for line in file:
+        line = line.split(",")
+        result[line[0]] = int(line[1])
+    return result
 
 
-def return_docids():
-    pass
+def return_docids(token:str, token_index:dict)->set:
+    position = token_index[token]
+    initial = token[0]
+    if initial.isdigit():
+        initial = "numeric"
+    path = directory + "/" + initial
+    file = open(path, "r")
+    file.seek(position)
+    file.readline()
+    temp = file.readline().strip()
+    print(temp)
+    file.close()
+    return set(temp)
+
+
+if __name__ == "__main__":
+    d = read_token_index()
+    print(return_docids("bren", d))
+    
