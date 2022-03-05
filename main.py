c5c3a06b99457f1ce9f04c28500c1d390664acce
zhihaoc9 <83797177+zhihaoc9@users.noreply.github.com>
2022-03-05 14:30:18 -0800

    add check exact duplicate

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     unique_links_dict = dict()
     unique_links_set = set()
     counter = 0
+    checksum_set = set()
     for entry in os.listdir(bp):
         x = os.path.join(bp, entry)
         for entry2 in os.listdir(x):
@@ -58,27 +59,35 @@ if __name__ == "__main__":
             encoding = json_dict['encoding']
             soup = BeautifulSoup(content, 'html.parser')
             text = soup.get_text()
-            tokens = tokenize(text)
-            for i in range(len(tokens)):
-                tokens[i] = stem(tokens[i])
             
-            tokens_freq = dict()
-            for i in range(len(tokens)):
-                if tokens[i] in tokens_freq:
-                    tokens_freq[tokens[i]].append(i)
-                else:
-                    tokens_freq[tokens[i]] = []
-                    tokens_freq[tokens[i]].append(i)
-                    
-                if i > 0:
-                    if tokens[i-1]+tokens[i] in tokens_freq:
-                        tokens_freq[tokens[i-1]+tokens[i]].append(i-1)
+            checksum = 0
+            for char in text:
+                checksum += int(ord(char))
+            if checksum not in checksum_set:
+                checksum_set.add(checksum)
+                tokens = tokenize(text)
+                for i in range(len(tokens)):
+                    tokens[i] = stem(tokens[i])
+                
+                tokens_freq = dict()
+                for i in range(len(tokens)):
+                    if tokens[i] in tokens_freq:
+                        tokens_freq[tokens[i]].append(i)
                     else:
-                        tokens_freq[tokens[i-1]+tokens[i]] = []
-                        tokens_freq[tokens[i-1]+tokens[i]].append(i-1)
+                        tokens_freq[tokens[i]] = []
+                        tokens_freq[tokens[i]].append(i)
+                        
+                    if i > 0:
+                        if tokens[i-1]+tokens[i] in tokens_freq:
+                            tokens_freq[tokens[i-1]+tokens[i]].append(i-1)
+                        else:
+                            tokens_freq[tokens[i-1]+tokens[i]] = []
+                            tokens_freq[tokens[i-1]+tokens[i]].append(i-1)
 
-            for k,positions in tokens_freq.items():
-                inverted_index.addUrlToToken(k,counter-1,len(positions), positions)
+                for k,positions in tokens_freq.items():
+                    inverted_index.addUrlToToken(k,counter-1,len(positions), positions)
+            else:
+                pass
 
     writeURLDict(unique_links_dict)
     print("NUMBER OF INDEXED DOCUMENTS: "+ str(len(unique_links_set)))
