b5056f8b4055db92d6005cd025844043ec79dbdc
Chris Chereque <ccherequ@uci.edu>
2022-03-07 00:19:45 -0800

    more position updates

--- qmain.py
+++ qmain.py
@@ -162,21 +162,67 @@ def query_tfidf(query, numDocs, doc_set, token_index):
 
     # Check which docs preserve positioning
 
+    # query_term_dist = [[term, (term2, dist), (term3, dist)], [term2, (term3, dist)], [term3]]
+    # relative_dist = [term, (term2, dist), (term3, dist)]
+    # term = term
+    # q_rel_dist = [(term2, dist), (term3, dist)]
+
+    # term_positions_list = [[term, (docid, [positions]), (docid2, []positions2)], [term2, (docid, [positions]), (docid2, []positions2)]]
+    # doc_dist = [term, (docid, [positions]), (docid2, [positions2])]
+    # doc_term = term
+    # doc_rel_dists = [(docid, [positions]), (docid2, [positions2])]
+
     for docid in doc_set:
-        q_rel_dists = []
         for relative_dist in query_term_distance:
-            term = relative_dist[0]
+            q_rel_dists = []
+            q_term = relative_dist[0]
             counter = 1
             while counter < len(relative_dist):
                 q_rel_dists.append(relative_dist[counter])
+                counter += 1
             if len(q_rel_dists) > 0:
-                doc_rel_dists = []
                 for doc_dist in term_positions_list:
+                    doc_rel_dists = []
                     doc_term = doc_dist[0]
-                    counter = 1
-                    while counter < len(doc_dist):
-                        doc_rel_dists.append(doc_dist[counter])
-                    
+                    if doc_term != q_term:
+                        
+
+                # ['car', , ('auto', 1), ('insur', 2), ('car', 3)]
+                # query_term_positions = (docid, [pos])
+                # t1
+                # term_positions = query_term_positions[1]
+
+                # for every other entry
+                #   t2
+                #   other_qterm_positions = (docid, [pos])
+                #   other_positions = other_qterm_positions[1]
+
+                #   [15,33]    [20]
+
+                #   c1 = 0
+                #   while c1 < len(term_positions)
+                #       c2 = 0
+                #       while c2 < len(other_positions)
+                #           if term_positions[c1] < other_positions[c2]
+                #               for tup in query_term_distance
+                #                   if tup[0] == t2:
+                #                       dist = tup[1]
+                #               if other_position[c2] - term_position[c1] == dist:
+                #                   true
+                #           c2 += 1
+                #       c1 += 1
+
+                """
+                for doc_dist in term_positions_list:
+                    doc_rel_dists = []
+                    doc_term = doc_dist[0]
+                    if doc_term == q_term:
+                        c = 1
+                        while counter < len(doc_dist):
+                            doc_rel_dists.append(doc_dist[c])
+                """
+
+
 
 
 
